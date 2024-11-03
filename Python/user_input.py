0c23dbeec9b4521c3f9bceedae87bd3c86b378ff
Levi Mugendi <128239439+LEVISHAWN@users.noreply.github.com>
2024-11-03 23:33:45 +0300

    a program that asks the user for their name, age, and location and then prints out a personalized message.

---
+++ Python/user_input.py
@@ -0,0 +1,8 @@
+# A program that asks the user for their name, age, and location and then prints out a personalized message.
+# Ask user for their name, age and location
+name = input("Your name: ")
+age = input("Your age: ")
+location = input("Your location: ")
+
+# Message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
