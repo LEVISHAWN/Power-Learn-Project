b08113b3730716958655fc236d4498c12dbd25a6
Levi Mugendi <128239439+LEVISHAWN@users.noreply.github.com>
2024-11-22 22:14:24 +0300

    Assignment 1 (Week 5: OOP): Design Your Own Class

---
+++ Python/book.py
@@ -0,0 +1,27 @@
+# Base class Book
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def read(self):
+        print(f"Reading the book '{self.title}' by {self.author}.")
+
+    def book_info(self):
+        print(f"Book: {self.title}, Author: {self.author}, Pages: {self.pages}")
+
+
+# class Ebook inheriting from Book
+class Ebook(Book):
+    def __init__(self, title, author, year, file_size, file_format):
+        super().__init__(title, author, year)
+        self.file_size = file_size
+        self.file_format = file_format
+
+    def download(self):
+        print(f"Downloading the Ebook '{self.title}'.")
+
+    def book_info(self):
+        super().book_info()
+        print(f"File Size: {self.file_size} MB\nFile Format: {self.file_format}")
